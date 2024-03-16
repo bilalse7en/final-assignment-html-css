d3306055f265fa55fd5b1f56a2eadcbc4cae63a7
Muhammad Bilal <bilal.qsols@gmail.com>
2024-03-16 06:59:39 +0500

    initial

---
+++ app.js
@@ -0,0 +1,14 @@
+new WOW().init();
+
+    // Counter Animation
+    $('.count').each(function () {
+        $(this).prop('Counter', 0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 3000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
